c8860ff3fb601d429d5020982e17dc74e3499ac4
elejunphant <jchanat@connect.ust.hk>
2025-01-25 22:01:22 +0800

    scene1 bubbly apples with sky and trees

---
+++ Assets/Scripts/Objects/BubbleTooHigh.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class DestroyOffScreen : MonoBehaviour
+{
+    private Camera mainCamera;
+    private Renderer objectRenderer;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        objectRenderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        if (objectRenderer.isVisible)
+        {
+            return;
+        }
+
+        Vector3 screenPoint = mainCamera.WorldToViewportPoint(transform.position);
+        if (screenPoint.y > 1 || screenPoint.y < 0 || screenPoint.x > 1 || screenPoint.x < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
