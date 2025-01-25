54422f14d3bbcb399f23cd56d180e61ae62030b9
elejunphant <jchanat@connect.ust.hk>
2025-01-26 04:45:34 +0800

    bubble apple basket ko

--- Assets/Scripts/Player/BubbleAppleBasket.cs
+++ Assets/Scripts/Player/BubbleAppleBasket.cs
@@ -2,18 +2,17 @@ using UnityEngine;
 
     public class BubbleAppleBasket : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D other)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("BubbleApple Trigger detected with " + other.gameObject.name);
 
-        if (other.gameObject.CompareTag("Basket"))
+        if (collision.gameObject.CompareTag("Basket"))
         {
-            Debug.Log("BubbleApple Trigger detected with Basket");
+            Debug.Log("Collision detected with Basket");
 
             // Instantiate the new prefab at the apple's position and rotation
             //Instantiate(newPrefab, transform.position, transform.rotation);
 
-            // Destroy the current BubbleApple GameObject
+            // Destroy the current apple GameObject
             Destroy(gameObject);
         }
     }
