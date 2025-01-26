de2814b41387a9f89587bd057e2acaa2a157310b
elejunphant <jchanat@connect.ust.hk>
2025-01-26 11:47:27 +0800

    BG music except final scene

--- Assets/Scripts/Meteor/WinTimer.cs
+++ Assets/Scripts/Meteor/WinTimer.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 
 public class WinTimer : MonoBehaviour
 {
-    public float winTime = 30;
+    public float winTime = 45;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
